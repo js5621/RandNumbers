a0e3b9668ee67d34e77334e853abf245bb25064c
js5621 <asaph5621@gmail.com>
2025-02-04 09:22:42 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Random = new Random();
+            int[] randArray = new int[52];
+            int randResultNum = 0;
+            for (int i = 0; i < randArray.Length; i++)
+            {
+                randArray[i] = i+1;
+            }
+
+            // 배열섞기
+            Random.Shuffle(randArray);
+
+            foreach (var item in randArray)
+            {
+                if(randResultNum == 8)
+                {
+                    break;
+                }
+                Console.WriteLine(item);
+                randResultNum++;
+            }
+       
+
+
+
+
+        }
+    }
+}
